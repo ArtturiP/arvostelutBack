054e866a37d40d05299643c368633edb576f7349
ArtturiP <artturi.polus@hotmail.com>
2023-08-31 16:05:14 +0300

    testiätestiä

--- server/juomaServer.js
+++ server/juomaServer.js
@@ -7,9 +7,11 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const cors = require('cors');
+app.use(cors());
 
 // Server port
-var HTTP_PORT = 8000 
+var HTTP_PORT = 3000 
 // Start server
 app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
